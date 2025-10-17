2540f02baf901b1a8fcc69de471868a36d36d165
rishiksehgal <rishik.sehgal10@gmail.com>
2025-10-17 12:30:23 -0700

    hi

--- MyStack.java
+++ MyStack.java
@@ -1,8 +1,22 @@
+/**
+ * MyStack mimics the actual Stack datastructure
+ * 
+ * @param <E> the type of objects that can be used in the Stack
+ * @author Rishik Sehgal
+ * No assistance    
+ * 
+ * @version Oct 7, 2025
+ * 
+ */
 public class MyStack<E>
 {
     private DoubleNode node;
 
-
+    /**
+     * Adds a node that becomes the head
+     * @param item is the value for the head node
+     * @return the value of the old head
+     */
     public E push(E item)
     {
         DoubleNode n = new DoubleNode(item);
@@ -17,11 +31,19 @@ public class MyStack<E>
             node = n;
         }
         n.setNext(null);
-        E x = (E) node.getValue();
-        node = n;
-        return x;
+        if(node != null)
+        {
+            E x = (E) node.getValue();
+            node = n;
+            return x;
+        }
+        return null;
     }
 
+    /**
+     * Gives the value of the head
+     * @return the value of the head
+     */
     public E peek()
     {
         if(isEmpty())
@@ -31,6 +53,10 @@ public class MyStack<E>
         return (E) node.getValue();
     }
 
+    /**
+     * Deletes the first heead and makes the next node the head
+     * @return the value of the deleted head
+     */
     public E pop()
     {
         E thing = (E) node.getValue();
@@ -42,14 +68,23 @@ public class MyStack<E>
         return thing;
     }
 
+    /**
+     * Determines if the stack is empty
+     * @return true if the stack is empty
+     */
     public boolean isEmpty()
     {
         return (node == null);
     }
 
+    /**
+     * Searches for an element in the Stack
+     * @param o the value being searched for 
+     * @return the index of the thing being searched from the head
+     */
     public int search(Object o)
     {
-        int x = 1;
+        int x = 0;
         DoubleNode n = node;
         while(n != null)
         {
@@ -64,6 +99,10 @@ public class MyStack<E>
         
     }
 
+    /**
+     * Returns the size of the Stack
+     * @return the size of the Stack
+     */
     public int size()
     {
         if(isEmpty())
---
+++ StringUtil.java
@@ -0,0 +1,80 @@
+import java.util.Stack;
+/**
+ * Adds some methods for a String
+ * @author Rishik Sehgal
+ * No assistance
+ * 
+ * @version Oct 15, 2025
+ */
+public class StringUtil
+{
+
+    /**
+     * Reverses the String given to it using Stacks
+     * @param str the string to be reversed
+     * @return the reversed string
+     */
+    public static String reverseString(String str)
+    {
+        Stack<String> stack = new Stack<>();
+        for (int i = 0; i < str.length(); i++) 
+        {
+            stack.push(str.substring(i, i + 1));
+        }
+        String x = "";
+        while (!stack.isEmpty()) 
+        {
+            x += stack.pop();
+        }
+        return x;
+    }
+
+    /**
+     * Determines if the String given is a palindrome
+     * @param str is the String being analyzed
+     * @return true if it is a palindrome
+     */
+    public static boolean isPalindrome(String str)
+    {
+        return str.equals(reverseString(str));
+    }
+
+    /**
+     * A tester method for the StringUtil class
+     * @param args random thingy
+     */
+    public static void main(String[] args)
+    {
+        String test =  "racecar";
+        String test2 = "notapalindrome";
+
+        if ( !("".equalsIgnoreCase(reverseString(""))) )
+        {
+            System.out.println("An empty string is palindrome");
+        }
+
+        if ( !("a".equalsIgnoreCase(reverseString("a"))) )
+        {
+            System.out.println("\"a\" is a palindrome");
+        }
+
+        if ( !("aa".equalsIgnoreCase(reverseString("aa"))) )
+        {
+            System.out.println("Error: \"aa\" is a palindrome");
+        }
+
+        if (!test.equalsIgnoreCase(reverseString(test)))
+        {
+            System.out.println("Error: " + test + " is a palindrome");
+        }
+        else
+        {
+            System.out.println("Success " + test + " matched " + reverseString(test));
+        }
+
+        if (test2.equalsIgnoreCase(reverseString(test2)))
+        {
+            System.out.println("Error: " + test2 + " is a palindrome");
+        }
+    }
+}
