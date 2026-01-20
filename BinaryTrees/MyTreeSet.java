be772cfcb8a998cf87637694cbcbb8be02f8f3a3
rishiksehgal <rishik.sehgal10@gmail.com>
2026-01-20 13:39:34 -0800

    h

--- BinaryTrees/MyTreeSet.java
+++ BinaryTrees/MyTreeSet.java
@@ -1,55 +1,86 @@
 import java.util.ArrayList;
 import java.util.Iterator;
 
+/**
+ * MyTreeSet mirrors a Tree Set.
+ * 
+ * @param <E> thing
+ * @author Rishik Sehgal
+ * @version 1
+ *
+ */ 
 public class MyTreeSet<E>
 {
-	private TreeNode root;
-	private int size;
-	private TreeDisplay display;
+    private TreeNode root;
+    private int size;
+    private TreeDisplay display;
 
-	public MyTreeSet()
-	{
-		size = 0;
-		display = new TreeDisplay();
-	}
+    /**
+     * Creates a MyTreeSet Object
+     */
+    public MyTreeSet()
+    {
+        size = 0;
+        display = new TreeDisplay();
+    }
 
-	public int size()
-	{
-		return size;
-	}
+    /**
+     * Returns the size of the TreeSet
+     * @return the size
+     */
+    public int size()
+    {
+        return size;
+    }
 
-	public boolean contains(Object obj)
-	{
-		Comparable  x = (Comparable) obj;
+    /**
+     * Determines if the TreeSet contains an object
+     * @param obj object being analyzed
+     * @return true if it has the object
+     */
+    public boolean contains(Object obj)
+    {
+        Comparable  x = (Comparable) obj;
         return BSTUtilities.contains(root, x, display);
-	}
+    }
 
-	// if obj is not present in this set, adds obj and
-	// returns true; otherwise returns false
-	public boolean add(E obj)
-	{
+
+    /**
+     * if obj is not present in this set, adds obj and
+     * returns true; otherwise returns false
+     * @param obj being added
+     * @return true if added
+     */
+    public boolean add(E obj)
+    {
         if(root == null)
         {
             root = new TreeNode(obj);
             size++;
             return true;
         }
-		if(contains(obj))
+        if(contains(obj))
         {
             return false;
         }
         else
         {
             size++;
-            root = BSTUtilities.insert(root,(Comparable) obj, display);
+            root = BSTUtilities.insert(root, (Comparable) obj, display);
             return true;
         }
-	}
+    }
 
-	// if obj is present in this set, removes obj and
-	// returns true; otherwise returns false}
-	public boolean remove(Object obj)
-	{
+    // if obj is present in this set, removes obj and
+    // returns true; otherwise returns false}
+    /**
+     * if obj is present in this set, removes obj and
+     * returns true; otherwise returns false}
+     * @param obj being removed
+     * @return true if removed
+     */
+    public boolean remove(Object obj)
+    {
         if(!contains(obj))
         {
             return false;
@@ -57,74 +88,109 @@ public class MyTreeSet<E>
         root = BSTUtilities.delete(root, (Comparable) obj, display);
         size--;
         return true;
-	}
+    }
 
-	public String toString()
-	{
-		return toString(root);
-	}
+    /**
+     * Returns the toString
+     * @return toString
+     */
+    public String toString()
+    {
+        return toString(root);
+    }
 
-	private String toString(TreeNode t)
-	{
-		if (t == null)
-			return " ";
-		return toString(t.getLeft()) + t.getValue() + toString(t.getRight());
-	}
+    /**
+     * Returns the toString
+     * @param t of a TreeSet
+     * @return the toString
+     */
+    private String toString(TreeNode t)
+    {
+        if (t == null)
+        {
+            return " ";
+        }
+        return toString(t.getLeft()) + t.getValue() + toString(t.getRight());
+    }
 
+    /**
+     * Returns an interator
+     * @return an iterator
+     */
     public TreeSetIterator iterator()
     {
         return new TreeSetIterator();
     }
 
+    /**
+    * TreeSetIterator for MyTreeSet class
+    * 
+    * @author Rishik Sehgal
+    * @version 1
+    *
+     */ 
     private class TreeSetIterator implements Iterator
     {
-        private ArrayList<Object> values = new ArrayList<Object>();
-        private int nextIndex = 0;
-        private int lastReturnedIndex = -1;
+        private ArrayList<Object> vals = new ArrayList<Object>();
+        private int next = 0;
+        private int last = -1;
 
+        /**
+         * Creates a TreeSetIterator Object
+         */
         public TreeSetIterator()
         {
-            inOrderTraversal(root);
+            inOrder(root);
         }
 
-        public void inOrderTraversal(TreeNode t)
+        /**
+         * Returns true if it has a next
+         * @return true if has next
+         */
+        public boolean hasNext()
+        {
+            return next < vals.size();
+        }
+
+        /**
+         * Adds the inOrder values in the vals arraylist.
+         * @param t tree being traversed
+         */
+        public void inOrder(TreeNode t)
         {
             if (t == null)
             {
                 return;
             }
-            inOrderTraversal(t.getLeft());
-            values.add(t.getValue());
-            inOrderTraversal(t.getRight());
+            inOrder(t.getLeft());
+            vals.add(t.getValue());
+            inOrder(t.getRight());
         }
         
-
-        public boolean hasNext()
-        {
-            return nextIndex < size;
-        }
-
-
-
-
+        /**
+         * Returns the Object from next and traverses
+         * @return the next Object
+         */
         public Object next()
         {
-            lastReturnedIndex = nextIndex;
-            int a = nextIndex;
-            nextIndex++;
-            return values.get(a);
+            last= next;
+            int temp = next;
+            next++;
+            return vals.get(temp);
         }
 
-
-
+        /**
+         * Removes the Object just returned by next
+         */
         public void remove()
         {
-            MyTreeSet.this.remove(lastReturnedIndex);
-            if (lastReturnedIndex < nextIndex)
+            MyTreeSet.this.remove(vals.get(last));
+            vals.remove(last);
+            if (last < next)
             {
-                nextIndex--;
+                next--;
             }
-            lastReturnedIndex = -1;
+            last = -1;
         }
     }
 }
